5fe6fe099cadfc2eff1cbb49d98b5c4a3c3f07f1
jinhyeon-dev <kjh101807@gmail.com>
2023-11-02 20:11:06 +0900

    finish

---
+++ src/OneDimensionalArray/Average.java
@@ -0,0 +1,13 @@
+package OneDimensionalArray;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            double nums = sc.nextDouble();
+        }
+    }
+}
--- src/OneDimensionalArray/MinMax.java
+++ src/OneDimensionalArray/MinMax.java
@@ -1,3 +1,5 @@
+package OneDimensionalArray;
+
 import java.util.Scanner;
 
 public class MinMax {
